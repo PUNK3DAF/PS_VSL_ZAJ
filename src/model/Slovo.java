bc13849b25d09c454f64297e8e1694f3780083de
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-11-02 15:55:25 +0100

    zajednicki

---
+++ src/model/Slovo.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author vldmrk
+ */
+public class Slovo implements Serializable {
+
+    private String karakter;
+    private int pozicija;
+
+    public Slovo() {
+    }
+
+    public Slovo(String karakter, int pozicija) {
+        this.karakter = karakter;
+        this.pozicija = pozicija;
+    }
+
+    public String getKarakter() {
+        return karakter;
+    }
+
+    public void setKarakter(String karakter) {
+        this.karakter = karakter;
+    }
+
+    public int getPozicija() {
+        return pozicija;
+    }
+
+    public void setPozicija(int pozicija) {
+        this.pozicija = pozicija;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Slovo other = (Slovo) obj;
+        if (this.pozicija != other.pozicija) {
+            return false;
+        }
+        return Objects.equals(this.karakter, other.karakter);
+    }
+
+    @Override
+    public String toString() {
+        return "Slovo{" + "karakter=" + karakter + ", pozicija=" + pozicija + '}';
+    }
+
+}
---
+++ src/transfer/ServerskiOdgovor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package transfer;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author vldmrk
+ */
+public class ServerskiOdgovor implements Serializable {
+
+    private Object odgovor;
+}
